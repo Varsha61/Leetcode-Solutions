baf4324c3e8f8cbcdcbaf4cb0dcf5a16082d700e
Varsha N <nvarsh403@gmail.com>
2026-08-12 11:37:47 +0530

    Create 147-InsertionSortList

---
+++ 147-InsertionSortList/147-InsertionSortList.java
@@ -0,0 +1,26 @@
+// Last updated: 8/12/2026, 11:37:47 AM
+class Solution{
+public ListNode insertionSortList(ListNode head) {
+        
+        if (head == null){
+            return head;
+        }
+        ListNode helper = new ListNode(0);
+        ListNode cur = head;
+        ListNode pre =helper;
+        ListNode next =null;
+        while(cur !=null){
+            next =cur.next;
+            while(pre.next!=null && pre.next.val<cur.val){
+                pre=pre.next;
+
+            }
+            cur.next=pre.next;
+            pre.next=cur;
+            pre=helper;
+            cur = next;
+        }
+        return  helper.next;
+    }
+}
+    
