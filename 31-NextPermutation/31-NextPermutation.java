71f8016101f791c5fcea9428c9b255122309c838
Varsha N <nvarsh403@gmail.com>
2026-08-12 11:39:55 +0530

    Create 31-NextPermutation

---
+++ 31-NextPermutation/31-NextPermutation.java
@@ -0,0 +1,36 @@
+// Last updated: 8/12/2026, 11:39:55 AM
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int index = -1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+        if(index==-1){
+            reverse(nums, 0, n);
+            return;
+        }
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>nums[index]){
+                int temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
+                break;
+            }
+        }
+        reverse(nums, index+1, n);
+    }
+    private static void reverse(int[] nums, int start, int end){
+        end--;
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
