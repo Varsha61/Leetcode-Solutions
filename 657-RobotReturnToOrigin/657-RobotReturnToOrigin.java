3cdaaa5093591697a7de45ea05ad2ca70651a690
Varsha N <nvarsh403@gmail.com>
2026-08-12 11:34:57 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,18 @@
+// Last updated: 8/12/2026, 11:34:57 AM
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+        for (char move : moves.toCharArray()) {
+            if (move == 'U')
+                y++;
+            else if (move == 'D')
+                y--;
+            else if (move == 'L')
+                x--;
+            else
+                x++;
+        }
+        return x == 0 && y == 0;
+    }
+}
