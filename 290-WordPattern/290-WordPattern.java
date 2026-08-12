715004d32033cfb89f2b363d1d0d5ab1b53c74b6
Varsha N <nvarsh403@gmail.com>
2026-08-12 11:36:28 +0530

    Create 290-WordPattern

---
+++ 290-WordPattern/290-WordPattern.java
@@ -0,0 +1,27 @@
+// Last updated: 8/12/2026, 11:36:28 AM
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        
+        char[] ch = pattern.toCharArray();
+        String[] str = s.split(" ");
+
+        if (ch.length != str.length)
+            return false;
+
+        Map<Character, String> map = new HashMap<>();
+
+        for (int i=0; i<str.length; i++) {
+
+            if (map.containsKey(ch[i])) {
+                if (!(map.get(ch[i]).equals(str[i]))) {
+                    return false;
+                }
+            }
+            else if (map.containsValue(str[i])) {
+                return false;
+            }
+            map.put(ch[i], str[i]);
+        }
+        return true;
+    }
+}
