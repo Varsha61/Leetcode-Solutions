c61d1865bdce78e987444a23fce0b6a1084ce3b1
Varsha N <nvarsh403@gmail.com>
2026-08-12 11:34:37 +0530

    Create 782-JewelsAndStones

---
+++ 782-JewelsAndStones/782-JewelsAndStones.java
@@ -0,0 +1,18 @@
+// Last updated: 8/12/2026, 11:34:37 AM
+import java.util.HashSet;
+
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        HashSet<Character> set = new HashSet<>();
+        for (char ch : jewels.toCharArray()) {
+            set.add(ch);
+        }
+        int count = 0;
+        for (char ch : stones.toCharArray()) {
+            if (set.contains(ch)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
