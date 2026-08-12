8e4abae19b4a013f2a3b0af5866703556b69938c
Varsha N <nvarsh403@gmail.com>
2026-08-12 11:36:22 +0530

    Create 338-CountingBits

---
+++ 338-CountingBits/338-CountingBits.java
@@ -0,0 +1,10 @@
+// Last updated: 8/12/2026, 11:36:22 AM
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+        return ans;
+    }
+}
